5d311b506e5e968fa7eec7aa367188a6a67ecff2
sidjou brondone <83036232+brandone22@users.noreply.github.com>
2021-08-07 00:34:49 +0200

    fist_game
    eco-game

---
+++ my-first-app/main.js
@@ -0,0 +1,21 @@
+// function entierAleatoire(min, max)
+// {
+//  return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+// function maFonction() {
+// var entier = entierAleatoire(0, 9);
+// var nombreUtilisateur = document.querySelector('#nombre').value;
+// var scoreJeu=document.querySelector("#score");
+// var score=parseInt(scoreJeu.textContent);
+// if(nombreUtilisateur==entier){
+//     score ++;
+//     scoreJeu.innerHTML=" "+ score;
+//     alert('vous avez trouvez le nombre aleatoire');
+
+// }else{
+//     alert('tu as rater');
+//     score ++;
+//     scoreJeu.innerHTML=" "+ score;
+// }
+    
+// }
